be6be19b959ebd815a4c55551521deab979ff8de
oliviaFadiga <olivia.fadiga.pa@hotmail.com>
2022-11-11 17:20:19 +0100

    multer cors header pour afficher les images

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const app = express();
 app.use(helmet());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
@@ -36,9 +37,8 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-app.use("/images", express.static(path.join(__dirname, "images")));
-
 app.use("/api/auth", userRoutes);
 app.use("/api/sauces", sauceRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
